dcd8cb62b00d2a188f6feab516b09190add9e558
Benjamin Milde <benni@kobrakai.de>
2016-12-04 00:04:45 +0100

    Little bit of refactoring with barba

--- web/static/js/barba.js
+++ web/static/js/barba.js
@@ -7,47 +7,49 @@ import SlideOutTransition from "./transitions/SlideOutTransition"
 
 import projectsStagger from "./transitions/ProjectsStagger"
 
-let logo = document.getElementById('headerimg')
+function init () {
+	let logo = document.getElementById('headerimg')
 
-const initializeLogoListeners = () => {
-	logo = document.getElementById('headerimg')
-
-	logo.addEventListener('mouseenter', () => {
-		if(Barba.HistoryManager.currentStatus().namespace === 'index') return
-		logo.src = logo.getAttribute('data-arrow')
-	}, false)
-
-	logo.addEventListener('mouseleave', () => {
+	function resetLogo() {
 		logo.src = logo.getAttribute('data-signee')
-	}, false)
-}
-
-const resetLogo = () => {
-	logo.src = logo.getAttribute('data-signee')
-}
-
-const animationTrigger = () => {
-	// let prev = Barba.HistoryManager.prevStatus() ? Barba.HistoryManager.prevStatus().namespace : null
-	let current = Barba.HistoryManager.currentStatus().namespace
-
-	if(current === 'index'){
-		projectsStagger()
-		resetLogo()
 	}
-}
 
-export default {
-	init () {
-		initializeLogoListeners()
+	function initializeLogoListeners() {
+		// Replace with back arrow on child pages
+		logo.addEventListener('mouseenter', () => {
+			if(Barba.HistoryManager.currentStatus().namespace === 'index') return
+			logo.src = logo.getAttribute('data-arrow')
+		}, false)
 
-		Barba.Pjax.start()
-		Barba.Prefetch.init()
-		Barba.Pjax.getTransition = () => {
-			return Barba.HistoryManager.prevStatus().namespace === 'index' 
-				? SlideInTransition 
-				: SlideOutTransition
+		// Revert image change
+		logo.addEventListener('mouseleave', resetLogo, false)
+	}
+
+	function animationTrigger() {
+		// let prev = Barba.HistoryManager.prevStatus() ? Barba.HistoryManager.prevStatus().namespace : null
+		let current = Barba.HistoryManager.currentStatus().namespace
+
+		if(current === 'index'){
+			projectsStagger()
+			resetLogo()
 		}
-		Barba.Dispatcher.on('transitionCompleted', animationTrigger)
-		animationTrigger()
 	}
+
+	// Start all the things
+	initializeLogoListeners()
+
+	Barba.Pjax.start()
+	Barba.Pjax.cacheEnabled = false
+	Barba.Prefetch.init()
+	Barba.Pjax.getTransition = () => {
+		return Barba.HistoryManager.prevStatus().namespace === 'index' 
+			? SlideInTransition 
+			: SlideOutTransition
+	}
+	Barba.Dispatcher.on('transitionCompleted', animationTrigger)
+	animationTrigger()
 }
+
+
+
+export default { init }
